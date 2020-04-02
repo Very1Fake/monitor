32668c4027e92f8069e171a3e783f848975923a0
very1fake <timurminer@gmail.com>
2020-04-02 16:46:39 +0300

    New codes

--- core/codes.py
+++ core/codes.py
@@ -1,21 +1,57 @@
 import struct
 
-_10001 = 'Thread started'
-_10002 = 'Thread paused'
-_10003 = 'Thread resumed'
-_10004 = 'Thread closing'
-_10005 = 'Thread closed'
-_11001 = 'Production mode enabled'
-_11002 = 'Signal Interrupt'
-_12001 = 'Collector initialized'
-_12002 = 'Collector started'
-_12003 = 'Collector was unexpectedly stopped'
-_12004 = 'Worker initialized'
-_12005 = 'Worker started'
-_12006 = 'Worker was unexpectedly stopped'
+# Debug (1xxxx)
+_13001 = 'N targets recived form script'
 _14001 = 'Executing target'
+_14002 = 'Target\'s execution result'
+
+# Information (2xxxx)
+_20001 = 'Thread started'
+_20002 = 'Thread paused'
+_20003 = 'Thread resumed'
+_20004 = 'Thread closing'
+_20005 = 'Thread closed'
+_21001 = 'Production mode enabled'
+_21002 = 'Signal Interrupt'
+_21003 = 'Turning off'
+_21004 = 'Saving success hashes started'
+_21005 = 'Saving success hashes complete'
+_21006 = 'Offline'
+_22001 = 'Collector initialized'
+_22002 = 'Collector started'
+_22003 = 'Collector was unexpectedly stopped'
+_22004 = 'Worker initialized'
+_22005 = 'Worker started'
+_22006 = 'Worker was unexpectedly stopped'
+_23001 = 'Reindexing parsers started'
+_23002 = 'Reindexing parsers complete'
+_24001 = 'Item available'
+_28001 = 'Log level changed'
+_28002 = 'Log mode changed'
+_28003 = 'Time changed to UTC'
+_28004 = 'Time changed to local'
+
+# Warnings (3xxxx)
+_33001 = 'Target lost while inserting in schedule'
 _34001 = 'Target lost'
-_41001 = 'ThreadManager unexpectedly has turned off'
+_34002 = 'Target lost in pipeline'
+_38001 = 'Meaningless level change (changing to the same value)'
+_38002 = 'Meaningless mode change (changing to the same value)'
+_38003 = 'Meaningless time change (changing to the same value)'
+
+# Errors (4xxxx)
+_43001 = 'Unknown index'
+_43002 = 'Wrong target list received from script'
+_43003 = 'Parser execution failed'
+_43004 = 'Target lost in pipeline (script unloaded)'
+_44001 = 'Unknown status received while executing'
+_44002 = 'Parser execution failed'
+_44003 = 'Target lost in pipeline (script unloaded)'
+_48001 = 'Can\'t change level (possible values (0, 1, 2, 3, 4, 5))'
+_48002 = 'Can\'t change mode (possible values (0, 1, 2, 3))'
+
+# Fatals (5xxxx)
+_51001 = 'ThreadManager unexpectedly has turned off'
 _52001 = 'Exception raised, emergency stop initiated'
 
 
@@ -24,9 +60,11 @@ class CodeError(Exception):
 
 
 class Code:
+    __slots__ = ('code', 'title', 'digest', 'hexdigest', 'message')
     code: int
-    digest: bytes
     title: str
+    digest: bytes
+    hexdigest: str
     message: str
 
     def __init__(self, code: int, message: str = ''):
@@ -39,10 +77,11 @@ class Code:
         else:
             raise CodeError('Code must be int')
         self.digest = struct.pack('>H', code)
+        self.hexdigest = hex(code)
         self.message = message
 
     def __str__(self) -> str:
-        return f'C{self.code}'
+        return self.format(1)
 
     def __repr__(self) -> str:
         return f'Code({self.code}, {self.digest}, {self.title})'
