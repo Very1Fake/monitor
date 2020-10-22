b1f8969ec0151985b219359099d915b1e6e066b4
very1fake <very1fake.coder@gmail.com>
2020-10-22 23:25:39 +0300

    Added Messages to API
    - Also EventHandler optimized;

--- source/__init__.py
+++ source/__init__.py
@@ -4,7 +4,7 @@ __copyright__ = "Copyright (c) 2020 Sellars"
 __credits__ = ["very1fake"]
 
 __license__ = "All Rights Reserved"
-__version__ = "1.0"
+__version__ = "1.0.1"
 __maintainer__ = "very1fake"
 
 version = Version(__version__)
