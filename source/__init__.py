2cf77cbe6602566e05d8da73d17c2ef4ac881d81
very1fake <very1fake.coder@gmail.com>
2021-03-15 21:31:17 +0300

    Issue #5: Remoter: Keywords commands fix

--- source/__init__.py
+++ source/__init__.py
@@ -4,7 +4,7 @@ __copyright__ = "Copyright (c) 2020 Sellars"
 __credits__ = ["very1fake"]
 
 __license__ = "All Rights Reserved"
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __maintainer__ = "very1fake"
 
 version = Version(__version__)
